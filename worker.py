b7861f447f686146f81850aa4a44c38989748376
Neil Dahlke <neil.dahlke@gmail.com>
2024-08-22 12:57:29 -0700

    Adding comments to the worker.

--- worker.py
+++ worker.py
@@ -1,20 +1,24 @@
 import asyncio
 import logging
 import os
-
 from temporalio.worker import Worker
 from shared import get_temporal_client
 from activities import ProvisioningActivities
 from workflows import ProvisionInfraWorkflow
 
+# Get the task queue name from the environment variable, defaulting to "provision-infra"
 TEMPORAL_TASK_QUEUE = os.environ.get("TEMPORAL_TASK_QUEUE", "provision-infra")
 
 async def main() -> None:
 	logging.basicConfig(level=logging.INFO)
 
+	# Get the Temporal client
 	client = await get_temporal_client()
+
+	# Create an instance of the ProvisioningActivities class
 	activities = ProvisioningActivities()
 
+	# Create a worker instance
 	worker: Worker = Worker(
 		client,
 		task_queue=TEMPORAL_TASK_QUEUE,
@@ -27,8 +31,11 @@ async def main() -> None:
 			activities.policy_check,
 		]
 	)
+
+	# Run the worker
 	await worker.run()
 
 
 if __name__ == "__main__":
+	# Run the main function using asyncio
 	asyncio.run(main())
